66408c5c9777783ee5cc31d1b83c4ef8440f413f
Aquzi <pacerfan1998@gmail.com>
2022-02-06 21:57:48 -0500

    Some viewmodel progress

--- app/src/main/java/com/bignerdranch/android/magic8ball/MainActivity.kt
+++ app/src/main/java/com/bignerdranch/android/magic8ball/MainActivity.kt
@@ -2,10 +2,8 @@ package com.bignerdranch.android.magic8ball
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.View
 import android.widget.Button
 import android.widget.TextView
-import androidx.lifecycle.ViewModelProvider
 import androidx.lifecycle.ViewModelProviders
 
 class MainActivity : AppCompatActivity() {
@@ -21,11 +19,12 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val provider: ViewModelProvider = ViewModelProviders.of(this)
-        val magic8BallViewModel = provider.get(Magic8BallViewModel::class.java)
+        ViewModelProviders.of(this).get(Magic8BallViewModel::class.java)
 
         triggerButton = findViewById(R.id.trigger_button)
         responseTextView = findViewById(R.id.response_text)
+
+        //responseTextView.setText(magic8BallViewModel.currentResponseText)
 
         triggerButton.setOnClickListener {
             getResponse()
@@ -35,7 +34,6 @@ class MainActivity : AppCompatActivity() {
    private fun getResponse(){
         val responseTextResId = magic8BallViewModel.currentResponseText
         responseTextView.setText(responseTextResId)
-        responseTextView.visibility = View.VISIBLE
     }
 
 }
